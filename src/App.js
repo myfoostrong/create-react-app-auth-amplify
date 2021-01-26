08fb15eecc214a081018a7e246f18f4841cf4a18
Conor <waconor@amazon.com>
2021-01-26 11:14:43 -0500

    Modified portal to interact with DynamoDB

--- src/App.js
+++ src/App.js
@@ -2,28 +2,98 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify, { Auth, API } from 'aws-amplify';
 import aws_exports from './aws-exports';
+import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
 Amplify.configure(aws_exports);
 
+async function createCheater() {
+  const data = {
+    body: {
+      id: "shit",
+      player_id: 123456,
+      anomaly: 1.0456,
+      fraud: 0
+    }
+  };
+  const apiData = await API.post('cheaterapi', '/cheater', data);
+  console.log({ apiData })
+  const moreData = await API.get('cheaterapi', '/cheater')
+  console.log({moreData})
+}
+
 class App extends Component {
+  intervalID;
+
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.intervalID)
+  }
+
+  getData = () => {
+    API.get('cheaterapi', '/cheater')
+    .then(response => {
+      this.setState({data: response})
+      this.intervalID = setTimeout(this.getData.bind(this), 5000);
+    })
+  }
+
+  banPlayer = (player) => {
+    player['banned'] = true
+    console.log(player)
+    API.put('cheaterapi', '/cheater', {body: player})
+    .then(response => console.log(response))
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Octank Cheater Management
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Nothing
-          </a>
+          
+        
         </header>
+        <Jumbotron className="App-Jumbotron">
+          
+
+          <Table striped bordered hover variant="dark" className="App-Table">
+            <thead>
+              <tr>
+                <th>Player ID</th>
+                <th>Anomaly Score</th>
+                <th>Cheat Detected</th>
+                <th>Banned</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((item)=>{
+                return (
+                  <tr key={item.player_id}>
+                    <td>{item.player_id}</td>
+                    <td>{item.anomaly}</td>
+                    <td>{item.fraud}</td>
+                    <td>{item.banned ? 'banned' : 'valid'}</td>
+                    <td><Button onClick={()=>this.banPlayer(item)}> Ban Player </Button></td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </Table>
+        </Jumbotron>
       </div>
     );
   }
